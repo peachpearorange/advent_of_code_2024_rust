1e1b9cd1bcf961820dbaa48653d9fc7b3d771aaf
I <>
2024-12-03 13:11:11 +0000

    .

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+#![allow(clippy::unnecessary_cast)]
+#![allow(unused_imports)]
+#![allow(dead_code)]
+// #![feature(const_trait_impl)]
+// #![feature(type_alias_impl_trait)]
+#![allow(unused_mut)]
+#![allow(unused_must_use)]
+#![allow(non_camel_case_types)]
+#![feature(variant_count)]
+#![feature(strict_overflow_ops)]
+#![feature(iter_intersperse)]
+#![feature(trivial_bounds)]
+#![feature(impl_trait_in_assoc_type)]
+// #![feature(impl_trait_existential_types)]
+// #![feature(option_get_or_insert_default)]
+#![feature(let_chains)]
+
+fn main() {
+  let current_day = day1;
+  dbg!(current_day());
+}
+use std::{fs::File,
+          io::{self, BufRead},
+          path::Path};
+
+fn day1() -> io::Result<()> {
+  // Define the relative path to your file
+  let relative_path = "input/input1";
+
+  // Open the file
+  let file_path = Path::new(relative_path);
+  let file = File::open(file_path)?;
+
+  // Create a buffered reader
+  let reader = io::BufReader::new(file);
+
+  // Read lines and parse them into a vector of arrays of numbers
+  let data: Vec<Vec<f64>> = reader.lines()
+                                  .filter_map(Result::ok) // Skip lines that failed to read
+                                  .map(|line| {
+                                    line.split_whitespace() // Split the line by spaces
+                                        .filter_map(|num| num.parse::<f64>().ok()) // Parse each number
+                                        .collect() // Collect into a Vec
+                                  })
+                                  .collect();
+
+  // Print the result
+  // println!("{:?}", data);
+
+  Ok(data)
+}
